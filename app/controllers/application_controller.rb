2e754685bbac63b212e9848c46bda17f3336187f
Lim Kimhuor <kimhuorlim@gmail.com>
2017-01-23 18:04:18 +0700

    add basic authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :authenticate
 
   def after_sign_in_path_for resource
     redirect_url resource
@@ -16,5 +17,13 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for resource
     resource == :user ? root_path : new_manager_session_path
+  end
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["USER_NAME"] && password == ENV["PASSWORD"]
+    end
   end
 end
